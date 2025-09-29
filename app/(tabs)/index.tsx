e00fc690662d3e7621c63851d4bf6b10a7d03fcd
Fab1029 <andres.ledesma@ucuenca.edu.ec>
2025-09-29 17:40:08 -0500

    (feature): Frontend SearchBar

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,51 @@
-import { View } from "react-native";
+import SearchBar from "@/components/SearchBar";
+import { colors } from "@/constants/colors";
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { useRouter } from "expo-router";
+import { Image, ScrollView, StyleSheet, View } from "react-native";
+
 
 export default function Index() {
+  const router = useRouter();
+
   return (
-    <View>
+    <View style={styles.homeContainer}> 
+      <Image source={images.bg} style={styles.imageBackground}/>
+
+      <ScrollView style={{flex: 1}} showsVerticalScrollIndicator={false}
+        contentContainerStyle={{minHeight: '100%', paddingBottom: 10 }}
+      >
+        <Image source={icons.logo} style={styles.logoIcon}/>
+
+        <View style = {{flex: 1, marginTop: 5}}>
+          <SearchBar 
+            placeholder= 'Search for a movie'
+            onPress={() => router.push('/search')}
+          />
+        </View>
+
+      </ScrollView>
+
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  homeContainer: {
+    flex: 1,
+    backgroundColor: colors.primary,
+  },
+  imageBackground: {
+    width: '100%',
+    height: '100%',
+    position: 'absolute'
+  },
+  logoIcon: {
+    width: 50,
+    height: 50,
+    marginTop: 30,
+    resizeMode: 'contain',
+    marginHorizontal: 'auto'
+  }
+})
---
+++ components/SearchBar.tsx
@@ -0,0 +1,49 @@
+import { colors } from '@/constants/colors';
+import { icons } from '@/constants/icons';
+import React from 'react';
+import { Image, StyleSheet, TextInput, View } from 'react-native';
+
+interface Props {
+    placeholder: string;
+    onPress: () => void;
+}
+
+const SearchBar = ({ placeholder, onPress}: Props) => {
+  return (
+    <View style={styles.searchBarContainer}>
+      <Image source={icons.search} style={styles.searchIcon}/>
+      <TextInput 
+        onPress={onPress}
+        placeholder={placeholder}
+        value=''
+        onChangeText={() => {}}
+        placeholderTextColor="#a8b5db"
+        style={styles.textInputSeacrhBar}
+
+      />
+    </View>
+  )
+}
+
+export default SearchBar
+
+const styles = StyleSheet.create({
+    searchBarContainer: {
+        padding: 7,
+        flexDirection: 'row',
+        alignItems: 'center',
+        borderRadius: 50,
+        backgroundColor: colors.dark[200]
+    },
+    searchIcon: {
+        width: 20,
+        marginLeft: 5,
+        tintColor: '#ab8bff',
+        resizeMode: 'contain',
+    },
+    textInputSeacrhBar: {
+        flex: 1,
+        marginLeft: 5,
+        color: 'white',
+    }
+})
